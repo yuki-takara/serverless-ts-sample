08d03e415131a41786d708b47d5ac855afdf36b7
yuki-takara <tkra.yuuki@gmail.com>
2020-07-22 16:27:59 +0900

    feat: helloの処理を参考記事に合わせて変更
    - 参考: [Serverless Framework\+TypeScriptのプロジェクトでnode\_modulesをAWS Lambda Layers化しよう \| Developers\.IO](https://dev.classmethod.jp/articles/serverless-framework-typescript-node-modules-to-lambda-layers/)

--- handler.ts
+++ handler.ts
@@ -1,4 +1,6 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
+import * as moment from 'moment'
+import { prune } from 'underscore.string'
 import 'source-map-support/register'
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
@@ -6,7 +8,10 @@ export const hello: APIGatewayProxyHandler = async (event, _context) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+        message: `Go Serverless Webpack (Typescript) v1.0! Your function executed successfully! ${prune(
+          moment().format(),
+          12
+        )}`,
         input: event,
       },
       null,
